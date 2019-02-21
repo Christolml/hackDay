a341f282d1e6efe6c97f9295e3a05ef6a15ae3a9
Christopher Emmanuelle Velazquez Castillo <christhopher.velazquez@4thsource.com>
2019-02-20 23:31:55 -0600

    Casi final

--- bowlingGame/main.go
+++ bowlingGame/main.go
@@ -7,61 +7,109 @@ import (
 )
 
 func main() {
+	scoreTotal :=  0
+	bonusStrike := 0
+	strike, spare := false, false
+	bandera := false
 
-	// insertando la semilla para mis números aleatorios
-	rand.Seed(time.Now().UnixNano())
-	scoreTotal := 0
 
 	for ronda := 0; ronda < 9; ronda++ {
 
-		tiro, scoreRound, pinosTotal := 0, 0, 10
-		//strike, spare := false, false
-		strike := false
+		scoreRound, pinosTotal := 0, 10
+
+		guardarScore := ""
+		var tiros[2]int
 
 		for turno := 0; turno < 2; turno++ {
 
-			tiro = rand.Intn(pinosTotal + 1)
+			tiro := generadorNumeros(pinosTotal)
+			tiros[turno] = tiro
 
 			if tiro == 10 {
-				//strike = true
-				//scoreTotal += 10
-				strikeNo()
+				strike = true
+
+				if bonusStrike == 2 {
+					bonusStrike = 1
+					bandera = true
+				} else {
+					bonusStrike = 2
+				}
+
+				scoreTotal += 10
+
+				if spare != true && bandera != true {
+					fmt.Println(tiro)
+				}
 				break
 			}
 
-			pinosTotal -= tiro
+			if bonusStrike != 0 {
+				guardarScore += fmt.Sprintf(" %d \n", tiro)
+				scoreTotal += tiro
+				bonusStrike--
+
+				if bonusStrike == 0 {
+					fmt.Println("STRIKE, score:", scoreTotal)
+
+					if bandera {
+						fmt.Println(10)
+						scoreTotal += 10
+						bandera = false
+						scoreTotal += scoreRound
+						bonusStrike = 1
+					} else {
+						fmt.Println(guardarScore)
+					}
+
+					scoreRound += tiro
+					if scoreRound == 10 {
+						spare = true
+					}
+					scoreTotal += scoreRound
+					strike = false
+					break
+				}
+			}
+
+			if spare == true {
+				scoreTotal += tiro
+				fmt.Println("SPARE, score:",scoreTotal)
+				spare = false
+			}
+
+			if strike == false && spare == false {
+				fmt.Println(tiro)
+				scoreTotal += tiro
+
+			}
 			scoreRound += tiro
 
-			//if scoreRound == 10 {
-			//
-			//	break
-			//}
+			if scoreRound == 10 {
+				spare = true
+			}
 
-			fmt.Println(tiro)
-
+			pinosTotal -= tiro
 		}
 
-		scoreTotal += scoreRound
-		if strike == false {
+
+
+		if spare == false && strike == false {
 			fmt.Println("Score: ", scoreTotal)
-		} else {
-			fmt.Println("Hiciste un STRIKE", scoreTotal)
+		} else if spare == true {
+			//fmt.Println("SPARE")
+		} else if strike == true {
+			//fmt.Println("STRIKE")
+		} else if strike == true && spare == true {
+			fmt.Println("SPARE, score:",scoreTotal)
+			spare = false
 		}
 	}
 }
 
 
-func spare() {
 
-}
-
-
-func strikeNo() {
-	fmt.Println("hola")
-}
-
-
-func generadorNumeros(rango int) {
-
-
+func generadorNumeros(rango int) int {
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(rango + 1)
+	return num
 }
